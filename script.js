d69c6a9f47f26d0dd58036ae3c2b2762b930790d
ethanhimes66 <ehimes3000@gmail.com>
2024-08-29 14:53:00 -0400

    Web UI add button functionality for Still video

--- script.js
+++ script.js
@@ -1,30 +1,18 @@
-//const popup = document.getElementById("popup");
-//const close = document.getElementById("close");
-//const videoPopup1 = document.getElementById("videoPopup1");
-//
-//popup.addEventListener("click", () => {
-//    videoPopup1.style.display = "block";
-//});
-//close.addEventListener("click", () => {
-//    videoPopup1.style.display = "none";
-//});
-
 function showAndPlayVideo() {
     // Show the video container
-    const videoContainer = document.getElementById('video-container');
+    const dillyVideoContainer = document.getElementById('dilly-video-container');
     const video = document.getElementById('dilly');
 
-    videoContainer.style.display = 'block';
+    dillyVideoContainer.style.display = 'block';
 
     video.play();
 }
 
-//function slideButtonContainer() {
-//    const videoContainer = document.getElementById('button-slider');
-//    videoContainer.style.transform = "translateY(75px)";
-//}
+function playStillVideo() {
+    const stillVideoContainer = document.getElementById('still-video-container');
+    const video = document.getElementById('still');
 
-function slideAndPlay() {
-//    slideButtonContainer();
-    showAndPlayVideo();
+    stillVideoContainer.style.display = 'block';
+
+    video.play();
 }
