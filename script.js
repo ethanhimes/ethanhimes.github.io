41b2b731196df9583e6e7a4f92bad3b05178ab28
ethanhimes66 <ehimes3000@gmail.com>
2024-11-07 14:54:36 -0500

    Web UI add Macarena video, moved video locations

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-function showAndPlayVideo() {
-    // Show the video container
-    const dillyVideoContainer = document.getElementById('dilly-video-container');
-    const video = document.getElementById('dilly');
+function playVideo(videoContainer, videoElement) {
+    videoContainer.style.display = 'block';
 
-    dillyVideoContainer.style.display = 'block';
-
-    video.play();
-}
-
-function playStillVideo() {
-    const stillVideoContainer = document.getElementById('still-video-container');
-    const video = document.getElementById('still');
-
-    stillVideoContainer.style.display = 'block';
-
-    video.play();
+    videoElement.play();
 }
