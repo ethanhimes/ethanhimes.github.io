f1468c045cfd95a1f0f08e6b36dff791a1ef6c4e
ethanhimes66 <ehimes3000@gmail.com>
2025-01-06 16:35:30 -0500

    Web UI add LeBron video, refactor JS function to disable video when clicked a 2nd time

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function playVideo(videoContainer, videoElement) {
-    videoContainer.style.display = 'block';
+//function playVideo(videoContainer, videoElement) {
+//    videoContainer.style.display = 'block';
+//
+//    videoElement.play();
+//}
 
-    videoElement.play();
+
+function playVideo(videoContainer, videoElement) {
+    if (videoContainer.style.display === 'block') {
+        videoElement.pause();
+        videoElement.currentTime = 0; // Reset the video to the start
+        videoContainer.style.display = 'none'; // Hide the video
+    } else {
+        videoContainer.style.display = 'block'; // Show the video
+        videoElement.play(); // Play the video
+    }
 }
